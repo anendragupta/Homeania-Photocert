8758f3f56ec8bd2dd14956f9a97a07bdb7b6165a
anendragupta <gupta.anendra@gmail.com>
2017-03-14 14:10:47 +0530

    Delete models.py

--- models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Practical_test(models.Model):
-    description = models.CharField(max_length=500)
-    image = models.ImageField(upload_to='\Users\guptaane\PycharmProjects\homeania\home\static\media')
